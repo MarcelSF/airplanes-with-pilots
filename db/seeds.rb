a1682a5632f975271a21f9010a9b2da75ccfc64a
Marcel Fonseca <marcelsf23br@gmail.com>
2020-10-08 14:41:41 -0300

    add simple seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Removing old planes from the Hangar"
+
+Airplane.destroy_all
+
+puts 'Creating cool airplanes!'
+
+flanker = Airplane.create(model: 'Flanker Su-30', crew: 'Pilot and RIO', image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Sukhoi_Su-30SM_in_flight_2014.jpg/1200px-Sukhoi_Su-30SM_in_flight_2014.jpg")
+
+airbus = Airplane.create(model: 'Airbus 320', crew: 'Pilot, Copilot, Flight Attendants', image_url: "https://i0.wp.com/www.aeroin.net/wp-content/uploads/2020/08/Airbus-A320-Laurent-ERRERA-20083001.jpg?fit=1024%2C683&ssl=1")
+
+bis = Airplane.create(model: '14 Bis', crew: 'Santos Dumont', image_url: "https://adslatin.com/wp-content/uploads/2019/07/santos-dumont-14-bis.jpeg")
