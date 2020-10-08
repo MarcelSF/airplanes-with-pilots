a4e3c6663ea32f21e4415c85141e12e3d0a384d0
Marcel Fonseca <marcelsf23br@gmail.com>
2020-10-08 15:34:33 -0300

    add show behavior

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -2,4 +2,8 @@ class AirplanesController < ApplicationController
   def index
     @airplanes = Airplane.all
   end
+
+  def show
+    @airplane = Airplane.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'airplanes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'airplanes/new', to: "airplanes#new", as: :new_airplane
+  get "airplanes/:id", to: "airplanes#show", as: :airplane
   get 'airplanes', to: "airplanes#index", as: :planes
 end
