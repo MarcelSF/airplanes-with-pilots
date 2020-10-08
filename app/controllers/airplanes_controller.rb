074ae93e7b97bc4520ebe95316b703356cf48611
Marcel Fonseca <marcelsf23br@gmail.com>
2020-10-08 19:36:08 -0300

    add new and create and simpleform

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -6,4 +6,20 @@ class AirplanesController < ApplicationController
   def show
     @airplane = Airplane.find(params[:id])
   end
+
+  def new
+    @airplane = Airplane.new
+  end
+
+  def create
+    @airplane = Airplane.new(airplane_params)
+    @airplane.save
+    redirect_to airplanes_path
+  end
+
+  private
+
+  def airplane_params
+    params.require(:airplane).permit(:model, :crew, :image_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'airplanes/new', to: "airplanes#new", as: :new_airplane
   get "airplanes/:id", to: "airplanes#show", as: :airplane
   get 'airplanes', to: "airplanes#index", as: :planes
+  post 'airplanes', to: "airplanes#create"
 end
