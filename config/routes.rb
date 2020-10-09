7e387e0a6862a011d91339624e7ce69fff9edc57
Marcel Fonseca <marcelsf23br@gmail.com>
2020-10-08 21:05:15 -0300

    all good

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'airplanes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'airplanes/new', to: "airplanes#new", as: :new_airplane
   get "airplanes/:id", to: "airplanes#show", as: :airplane
