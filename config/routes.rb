92abf949291957596530baecd6e63947048cb713
Marcel Fonseca <marcelsf23br@gmail.com>
2020-10-11 19:09:43 -0300

    add airplane partial and rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'airplanes#index'
   get 'airplanes/new', to: "airplanes#new", as: :new_airplane
   get "airplanes/:id", to: "airplanes#show", as: :airplane
   get 'airplanes', to: "airplanes#index", as: :planes
